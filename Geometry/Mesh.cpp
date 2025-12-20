e6fb8d51982b058f7cab6abf494bfb53ebb5cb2d
Sahed Saad <sahedsaad2006@gmail.com>
2025-12-19 23:01:02 -0500

    n/a

--- Geometry/Mesh.cpp
+++ Geometry/Mesh.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// Default Constructor
 Mesh::Mesh(){
     vector<Vector3> v(8);
     vector<Triangle> t(24);
@@ -12,6 +13,7 @@ Mesh::Mesh(){
     screenTriangles = s;
 }
 
+// Constructor
 Mesh::Mesh(vector<Vector3> v, vector<Triangle> t){
     vector<ScreenTriangle> s(t.size());
     vertices = v;
@@ -19,12 +21,14 @@ Mesh::Mesh(vector<Vector3> v, vector<Triangle> t){
     screenTriangles = s;
 }
 
+// Get the local center of the 3d model for easier rotation
 void Mesh::ComputeMeshCenter(){
     Vector3 center;
     if (vertices.empty()) {
         meshCenter = center;
         return;
     }
+    // Sum all xyz coords to get average after
     for(int i = 0; i < vertices.size(); i++){
         center.x += vertices.at(i).x;
         center.y += vertices.at(i).y;
@@ -33,13 +37,16 @@ void Mesh::ComputeMeshCenter(){
     meshCenter = center.MultiplyVector(1.0f / vertices.size());
 }
 
+// Find the best matching triangle when user in inspector mode clicks LMB
 void Mesh::HighlightTriangle(Vector2 screenPoint){
     int bestMatchIdx = -1;
     float bestZDepth = 1000000000;
     for(int i = 0; i < screenTriangles.size(); i++){
         ScreenTriangle t = screenTriangles.at(i);
+        // check every ScreenTriangle
         if(t.IsPointInTriangle(screenPoint)){
             float z = t.GetAverageDepth();
+            // The least depth is the closest to the camera
             if(z < bestZDepth){
                 bestZDepth = z;
                 bestMatchIdx = t.triangleIndex;
